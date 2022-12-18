1640222041e896ba77f9acf9c597d5536924d73e
kendzo <psogen@yandex.ru>
2022-12-19 01:42:05 +0300

    interface

--- adonisface/Form1.Designer.cs
+++ adonisface/Form1.Designer.cs
@@ -129,7 +129,7 @@
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "adonisface";
             this.ResumeLayout(false);
             this.PerformLayout();
 
