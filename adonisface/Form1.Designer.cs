c3bb389e985a51db9ae6a546c442ed645ea7dde9
kendzo <psogen@yandex.ru>
2022-12-19 01:44:06 +0300

    zxc

--- adonisface/Form1.Designer.cs
+++ adonisface/Form1.Designer.cs
@@ -42,7 +42,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(35, 75);
+            this.label1.Location = new System.Drawing.Point(35, 64);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(317, 16);
             this.label1.TabIndex = 0;
