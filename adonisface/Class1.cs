7fbc78670a480de2e55fca794ac665a32b01fbdd
kendzo <psogen@yandex.ru>
2022-12-19 10:00:19 +0300

    logicofclass

---
+++ adonisface/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace adonisface
+{
+    internal class Class1
+    {
+        static public double X1(double a, double b, double c)
+        {
+            double disc = Math.Pow(b, 2) - 4 * a * c;
+            if disc     }
+    }
+}
