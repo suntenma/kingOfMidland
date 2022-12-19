57e0f2909fe856de65055132c15c532680d5e09c
kendzo <psogen@yandex.ru>
2022-12-19 10:03:04 +0300

    zxc

--- adonisface/Class1.cs
+++ adonisface/Class1.cs
@@ -11,6 +11,25 @@ namespace adonisface
         static public double X1(double a, double b, double c)
         {
             double disc = Math.Pow(b, 2) - 4 * a * c;
-            if disc     }
+            if (disc > 0)
+            {
+                double x1 = (-b - Math.Sqrt(disc)) / 2 * a;
+                return x1;
+            }
+            else 
+                return 0;
+        }
+
+        static public double X2(double a, double b, double c)
+        {
+            double disc = Math.Pow(b, 2) - 4 * a * c;
+            if (disc > 0)
+            {
+                double x1 = (-b + Math.Sqrt(disc)) / 2 * a;
+                return x1;
+            }
+            else
+                return 0;
+        }
     }
 }
