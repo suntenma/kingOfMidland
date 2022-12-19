c4f21b4959a2402656acd7b2f8775d1e02655091
kendzo <psogen@yandex.ru>
2022-12-19 10:13:45 +0300

    logicofform

--- adonisface/Form1.cs
+++ adonisface/Form1.cs
@@ -15,6 +15,24 @@ namespace adonisface
         public Form1()
         {
             InitializeComponent();
+            label5.Visible = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double a, b, c;
+            a = double.Parse(textBox1.Text);
+            b = double.Parse(textBox2.Text);
+            c = double.Parse(textBox3.Text);
+
+            label5.Visible = true;
+
+            if(Class1.X1(a, b, c) == Class1.X2(a, b, c))
+            {
+                label5.Text = "answer: " + $"x = {Class1.X1(a, b, c)};
+            }
+            else
+                label5.Text = "answer: " + $"x1 = {Class1.X1(a, b, c)}, x2 = {Class1.X2(a, b, c)}";
         }
     }
 }
